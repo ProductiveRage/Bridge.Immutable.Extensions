2ae048b0ae7a334f11c3e4c15f7353720e1d8296
ProductiveRage <dangger36@gmail.com>
2016-05-09 21:13:21 +0100

    Added more information to the ToSet extension method

--- Bridge.Immutable.Extensions/IEnumerableExtensions.cs
+++ Bridge.Immutable.Extensions/IEnumerableExtensions.cs
@@ -10,6 +10,11 @@ namespace Bridge.Immutable.Extensions
 		// I didn't include this in Bridge/ProductiveRage.Immutable because it's most efficient to start with an empty Set
 		// and Insert items into the start of it - this is a very cheap operation. But this is very convenient and so it
 		// makes sense to declare it somewhere.
+		/// <summary>
+		/// This will throw an exception for any null references (either the values set or any reference within that set)
+		/// since the Set type will not accept any nulls (if it must potentially contain missing values then type T should
+		/// be an Optional of some type)
+		/// </summary>
 		public static Set<T> ToSet<T>(this IEnumerable<T> values)
 		{
 			if (values == null)
