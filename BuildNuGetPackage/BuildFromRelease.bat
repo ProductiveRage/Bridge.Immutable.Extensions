150575fcf04df8a5f20d1f519bd34b9aa04439f6
ProductiveRage <dangger36@gmail.com>
2016-05-10 14:44:11 +0100

    Corrected oversigh in Build-NuGet batch file after rename from Bridge.Immutable to ProductiveRage.Immutable

--- BuildNuGetPackage/BuildFromRelease.bat
+++ BuildNuGetPackage/BuildFromRelease.bat
@@ -8,8 +8,8 @@ del *.dll
 del *.pdb
 del *.xml
 
-copy ..\Bridge.Immutable.Extensions\bin\Release\ProductiveRage.Immutable.Extensions.dll > nul
-copy ..\Bridge.Immutable.Extensions\bin\Release\ProductiveRage.Immutable.Extensions.xml > nul
+copy ..\ProductiveRage.Immutable.Extensions\bin\Release\ProductiveRage.Immutable.Extensions.dll > nul
+copy ..\ProductiveRage.Immutable.Extensions\bin\Release\ProductiveRage.Immutable.Extensions.xml > nul
 
 copy ..\ProductiveRage.Immutable.Extensions.nuspec > nul
 ..\packages\NuGet.CommandLine.3.4.3\tools\nuget pack -NoPackageAnalysis ProductiveRage.Immutable.Extensions.nuspec
