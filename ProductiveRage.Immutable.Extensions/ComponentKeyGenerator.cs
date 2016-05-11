aab064eee5d335b66e6732c7bb5889cc80b83529
ProductiveRage <dangger36@gmail.com>
2016-05-11 21:03:37 +0100

    Added RequestId and ComponentKeyGenerator classes - common in React / Flux-like applications

---
+++ ProductiveRage.Immutable.Extensions/ComponentKeyGenerator.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Bridge;
+
+namespace ProductiveRage.Immutable
+{
+	public static class ComponentKeyGenerator
+	{
+		private static DateTime _timeOfLastId = DateTime.MinValue;
+		private static int _offsetOfLastId = 0;
+
+		/// <summary>
+		/// When declaring dynamic child components, each should one should have a consistent unique key. For some components, this is easy - eg. if rendering a list of
+		/// messages that have been persisted on the server, each message is likely to have a unique id that may be used as the key. However, there are also times when
+		/// a dynamic list of items may be created and managed where items may be added and removed (which prevents a simple incrementing numeric value to be used)
+		/// where there is no "persistence id" to rely upon. This function will generate a new key that is guaranteed to be unique from any other keys that it
+		/// has returned (so it is not safe to have a set of child components where some use keys from here and some use keys from elsewhere).
+		/// </summary>
+		public static Any<string, int> GetNew()
+		{
+			// This uses the same logic as the RequestId, but we can't use just re-use the RequestId's code since it doesn't expose its internal requestTime and
+			// requestOffset values. Also, we may want to change this logic in the future.
+			var requestTime = DateTime.Now;
+			if (_timeOfLastId < requestTime)
+			{
+				_offsetOfLastId = 0;
+				_timeOfLastId = requestTime;
+			}
+			else
+				_offsetOfLastId++;
+
+			return string.Format(
+				"{0}-{1}",
+				requestTime.GetTime(),
+				_offsetOfLastId
+			);
+		}
+	}
+}
---
+++ ProductiveRage.Immutable.Extensions/RequestId.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace ProductiveRage.Immutable
+{
+	/// <summary>
+	/// It is a common pattern in a Flux-like architecture for API calls to initiate work and for an action to be sent to the Dispatcher when
+	/// that work has completed, as all other communications are handled (rather than using the Dispatcher for some events and other async
+	/// mechanisms, such as Promises, for other communications). It is often important to be able to identify a particular work-completed
+	/// action back to the where the API call was made (for example, if a form's content is to be saved then that form may be displayed in
+	/// a disabled state until that particular save operation has completed) or for a particular work-completed action to be compared to
+	/// another, to determine which is more recent (if there are multiple ways in which new data may arrive in an application, it is useful
+	/// to be able to ignore stale data that arrives after more recent data). A new RequestId instance is guaranteed to be unique, while
+	/// still being comparable to other instances to determine which was created more recently. The RequestId is useful as the return
+	/// value for API calls, so the caller keeps the RequestId and then waits for an action to arrive that corresponds to it.
+	/// </summary>
+	public sealed class RequestId
+	{
+		private static DateTime _timeOfLastId = DateTime.MinValue;
+		private static int _offsetOfLastId = 0;
+
+		private readonly DateTime _requestTime;
+		private readonly int _requestOffset;
+		public RequestId()
+		{
+			_requestTime = DateTime.Now;
+			if (_timeOfLastId < _requestTime)
+			{
+				_offsetOfLastId = 0;
+				_timeOfLastId = _requestTime;
+			}
+			else
+				_offsetOfLastId++;
+			_requestOffset = _offsetOfLastId;
+		}
+
+		public bool ComesAfter(RequestId other)
+		{
+			if (other == null)
+				throw new ArgumentNullException("other");
+
+			if (_requestTime == other._requestTime)
+				return _requestOffset > other._requestOffset;
+			return (_requestTime > other._requestTime);
+		}
+
+		public bool IsEqualToOrComesAfter(Optional<RequestId> other)
+		{
+			// If the "other" reference is no-RequestId then the "source" may be considered to come after it
+			if (!other.IsDefined)
+				return true;
+
+			return (other.Value == this) || this.ComesAfter(other.Value);
+		}
+	}
+}
