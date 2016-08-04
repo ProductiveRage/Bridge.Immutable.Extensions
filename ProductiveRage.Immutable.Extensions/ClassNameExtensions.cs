89a5e555090c0e0007862df10987df93307eabd3
ProductiveRage <dangger36@gmail.com>
2016-08-04 22:14:03 +0100

    Extended ClassName / Optional<ClassName> Add extension methods to return non-Optional value where it may be guaranteed to have a value
    Also added the warning about missing xml summary comments on public
    members to the ignore list, not everything needs a description

---
+++ ProductiveRage.Immutable.Extensions/ClassNameExtensions.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace ProductiveRage.Immutable
+{
+	public static class ClassNameExtensions
+	{
+		/// <summary>
+		/// Combine a ClassName instance with an Optional ClassName instance, if the Optional ClassName has a value - otherwise the ClassName will be
+		/// returned. If the Optional ClassName has a value then the two will be joined by a single space.
+		/// </summary>
+		public static ClassName Add(this ClassName source, Optional<ClassName> other)
+		{
+			if (source == null)
+				throw new ArgumentNullException("source");
+
+			return source.Add(" ", other);
+		}
+
+		/// <summary>
+		/// Combine a ClassName instance with an Optional ClassName instance, if the Optional ClassName has a value - otherwise the ClassName will be
+		/// returned. If the Optional ClassName has a value then the two will be joined by a specified delimiter string (which may be blank if desired).
+		/// </summary>
+		public static ClassName Add(this ClassName source, string delimiter, Optional<ClassName> other)
+		{
+			if (delimiter == null)
+				throw new ArgumentNullException("delimiter");
+
+			if (!other.IsDefined)
+				return source;
+
+			return new ClassName(source.Value + delimiter + other.Value.Value);
+		}
+
+		/// <summary>
+		/// Combine two ClassNames, joining their values with a single space
+		/// </summary>
+		public static ClassName Add(this ClassName source, ClassName other)
+		{
+			if (source == null)
+				throw new ArgumentNullException("source");
+			if (other == null)
+				throw new ArgumentNullException("other");
+
+			return source.Add(" ", other);
+		}
+
+		/// <summary>
+		/// Combine two ClassNames, joining their values with a specified delimiter string (which may be blank if desired)
+		/// </summary>
+		public static ClassName Add(this ClassName source, string delimiter, ClassName other)
+		{
+			if (source == null)
+				throw new ArgumentNullException("source");
+			if (delimiter == null)
+				throw new ArgumentNullException("delimiter");
+			if (other == null)
+				throw new ArgumentNullException("other");
+
+			return new ClassName(source.Value + delimiter + other.Value);
+		}
+	}
+}
--- ProductiveRage.Immutable.Extensions/OptionalClassNameExtensions.cs
+++ ProductiveRage.Immutable.Extensions/OptionalClassNameExtensions.cs
@@ -32,5 +32,35 @@ namespace ProductiveRage.Immutable
 
 			return new ClassName(source.Value.Value + delimiter + other.Value.Value);
 		}
+
+		/// <summary>
+		/// Combine an Optional ClassName instance with a non-Optional ClassName - if the first instance has a value then the two will be combined, separated
+		/// by a single space. If the source Optional ClassName has no value then the non Optional ClassName will be returned.
+		/// </summary>
+		public static ClassName Add(this Optional<ClassName> source, ClassName other)
+		{
+			if (other == null)
+				throw new ArgumentNullException("other");
+
+			return source.Add(" ", other);
+		}
+
+		/// <summary>
+		/// Combine an Optional ClassName instance with a non-Optional ClassName - if the first instance has a value then the two will be combined, separated
+		/// by a specified delimiter string (which may be blank if desired). If the source Optional ClassName has no value then the non Optional ClassName
+		/// will be returned.
+		/// </summary>
+		public static ClassName Add(this Optional<ClassName> source, string delimiter, ClassName other)
+		{
+			if (delimiter == null)
+				throw new ArgumentNullException("delimiter");
+			if (other == null)
+				throw new ArgumentNullException("other");
+
+			if (!source.IsDefined)
+				return other;
+
+			return new ClassName(source.Value.Value + delimiter + other.Value);
+		}
 	}
 }
