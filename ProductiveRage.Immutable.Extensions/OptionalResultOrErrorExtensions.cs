975208d3e036e7a5807a9d547c783942c21687d4
ProductiveRage <dangger36@gmail.com>
2017-04-29 22:36:40 +0100

    Added ResultOrError class, which has a lot of applications (along with extension methods for Optional<ResultOrError>)

---
+++ ProductiveRage.Immutable.Extensions/OptionalResultOrErrorExtensions.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using Bridge;
+using Bridge.React;
+
+namespace ProductiveRage.Immutable
+{
+	public static class OptionalResultOrErrorExtensions
+	{
+		/// <summary>
+		/// It is common to want to match an Optional Result-or-Errort to one of three ReactElements (a loading state, an error state and a result state) but if all three delegates are specified as anonymous
+		/// lambdas and they all return custom components (that are derived from Bridge.React's Component, PureComponent or StatelessComponent) then the compiler will not be able to infer a common base type
+		/// for them and so type inference on the two-type-argument Match method will fail. Having this method signature gives the compiler a big enough clue that it can work out that common case.
+		/// </summary>
+		public static ReactElement Match<T>(this Optional<ResultOrError<T>> source, ReactElement handleNoValue, Func<T, ReactElement> handleResult, Func<NonBlankTrimmedString, ReactElement> handleError)
+		{
+			if (handleNoValue == null)
+				throw new ArgumentNullException(nameof(handleNoValue));
+			if (handleResult == null)
+				throw new ArgumentNullException(nameof(handleResult));
+			if (handleError == null)
+				throw new ArgumentNullException(nameof(handleError));
+
+			if (!source.IsDefined)
+				return handleNoValue;
+
+			return source.Value.Match(handleResult, handleError);
+		}
+
+		public static TResult Match<T, TResult>(this Optional<ResultOrError<T>> source, TResult handleNoValue, Func<T, TResult> handleResult, Func<NonBlankTrimmedString, TResult> handleError)
+		{
+			if (handleNoValue == null)
+				throw new ArgumentNullException(nameof(handleNoValue));
+			if (handleResult == null)
+				throw new ArgumentNullException(nameof(handleResult));
+			if (handleError == null)
+				throw new ArgumentNullException(nameof(handleError));
+
+			if (!source.IsDefined)
+				return handleNoValue;
+
+			return source.Value.Match(handleResult, handleError);
+		}
+
+		public static TResult Match<T, TResult>(this Optional<ResultOrError<T>> source, Func<TResult> handleNoValue, Func<T, TResult> handleResult, Func<NonBlankTrimmedString, TResult> handleError)
+		{
+			if (handleNoValue == null)
+				throw new ArgumentNullException(nameof(handleNoValue));
+			if (handleResult == null)
+				throw new ArgumentNullException(nameof(handleResult));
+			if (handleError == null)
+				throw new ArgumentNullException(nameof(handleError));
+
+			if (!source.IsDefined)
+				return handleNoValue();
+
+			return source.Value.Match(handleResult, handleError);
+		}
+
+		public static void Match<T>(this Optional<ResultOrError<T>> source, Action handleNoValue, Action<T> handleResult, Action<NonBlankTrimmedString> handleError)
+		{
+			if (handleNoValue == null)
+				throw new ArgumentNullException(nameof(handleNoValue));
+			if (handleResult == null)
+				throw new ArgumentNullException(nameof(handleResult));
+			if (handleError == null)
+				throw new ArgumentNullException(nameof(handleError));
+
+			if (!source.IsDefined)
+				handleNoValue();
+			else
+				source.Value.Match(handleResult, handleError);
+		}
+
+		/// <summary>
+		/// If this instance represents a Result then that Result will be transformed using the provided delegate, creating a new ResultOrError (potentially wrapping a different type) unless
+		/// the new value is the same as the current value, in which case the current ResultOrError instance will be returned (there would be no need to create a new one). If this instance
+		/// represents an Error then this will return a ResultOrError instance for the mapper's destination type, maintaining the same error information (unless the destination type is the
+		/// same as the current type, in which case the current instance may be directly returned). If this is a missing Optional value then a missing Optional for the destination type will
+		/// be returned.
+		/// </summary>
+		public static Optional<ResultOrError<TDestination>> Map<T, TDestination>(this Optional<ResultOrError<T>> source, Func<T, TDestination> mapper)
+		{
+			if (mapper == null)
+				throw new ArgumentNullException(nameof(mapper));
+
+			if (!source.IsDefined)
+			{
+				// If the destination type is the same as the current type then we don't need a new instance (but the C# compiler won't let us do a cast so we have to Script.Write it)
+				if (typeof(T) == typeof(TDestination))
+					return Script.Write<Optional<ResultOrError<TDestination>>>("source");
+				return Optional<ResultOrError<TDestination>>.Missing;
+			}
+
+			return source.Value.Map(mapper);
+		}
+
+		/// <summary>
+		/// To deal correctly with all of the no-value, error, result states it is strongly recommended that the Match methods be used but there are some times where you just want to be
+		/// able to say give me the if you have it (and Missing if not). On those occasions, this method may be used.
+		/// </summary>
+		public static Optional<T> TryToGetResult<T>(this Optional<ResultOrError<T>> value)
+		{
+			return value.Match(
+				handleNoValue: Optional<T>.Missing,
+				handleError: error => Optional<T>.Missing,
+				handleResult: result => Optional.For(result) // Can't rely upon implicit cast due to https://forums.bridge.net/forum/bridge-net-pro/bugs/4049)
+			);
+		}
+	}
+}
---
+++ ProductiveRage.Immutable.Extensions/ResultOrError.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using Bridge;
+
+namespace ProductiveRage.Immutable
+{
+	public static class ResultOrError
+	{
+		public static ResultOrError<T> FromResult<T>(T result)
+		{
+			return ResultOrError<T>.FromResult(result);
+		}
+	}
+
+	/// <summary>
+	/// This represents EITHER a result or an error message - never both and never neither. It does not directly expose the Result and Error properties since they encourages code based upon conditionals
+	/// that are very easy to write such that they only handle one case or the other. Instead, this class has Match methods which must be provided delegates to deal with both the result AND the error cases.
+	/// </summary>
+	public sealed class ResultOrError<T>
+	{
+		public static ResultOrError<T> FromResult(T result)
+		{
+			if (result == null)
+				throw new ArgumentNullException(nameof(result));
+			return new ResultOrError<T>(result, null);
+		}
+		public static ResultOrError<T> FromError(NonBlankTrimmedString errorMessage)
+		{
+			if (errorMessage == null)
+				throw new ArgumentNullException(nameof(errorMessage));
+			return new ResultOrError<T>(Optional<T>.Missing, errorMessage);
+		}
+
+		private readonly Optional<T> _result;
+		private readonly Optional<NonBlankTrimmedString> _errorMessage;
+		private ResultOrError(Optional<T> result, Optional<NonBlankTrimmedString> errorMessage)
+		{
+			if ((result.IsDefined && errorMessage.IsDefined) || (!result.IsDefined && !errorMessage.IsDefined))
+				throw new ArgumentException($"Precisely one of {nameof(result)} and {nameof(errorMessage)} must have a value");
+
+			_result = result;
+			_errorMessage = errorMessage;
+		}
+
+		public TResult Match<TResult>(Func<T, TResult> handleResult, Func<NonBlankTrimmedString, TResult> handleError)
+		{
+			if (handleResult == null)
+				throw new ArgumentNullException(nameof(handleResult));
+			if (handleError == null)
+				throw new ArgumentNullException(nameof(handleError));
+
+			return _result.IsDefined ? handleResult(_result.Value) : handleError(_errorMessage.Value);
+		}
+
+		public void Match(Action<T> handleResult, Action<NonBlankTrimmedString> handleError)
+		{
+			if (handleResult == null)
+				throw new ArgumentNullException(nameof(handleResult));
+			if (handleError == null)
+				throw new ArgumentNullException(nameof(handleError));
+
+			if (_result.IsDefined)
+				handleResult(_result.Value);
+			else
+				handleError(_errorMessage.Value);
+		}
+
+		/// <summary>
+		/// If this instance represents a Result then that Result will be transformed using the provided delegate, creating a new ResultOrError (potentially wrapping a different type) unless
+		/// the new value is the same as the current value, in which case the current ResultOrError instance will be returned (there would be no need to create a new one). If this instance
+		/// represents an Error then this will return a ResultOrError instance for the mapper's destination type, maintaining the same error information (unless the destination type is the
+		/// same as the current type, in which case the current instance may be directly returned).
+		/// </summary>
+		public ResultOrError<TDestination> Map<TDestination>(Func<T, TDestination> mapper)
+		{
+			if (mapper == null)
+				throw new ArgumentNullException(nameof(mapper));
+
+			if (!_result.IsDefined)
+			{
+				if (typeof(T) == typeof(TDestination))
+				{
+					// If the destination type is the same as the current type then we don't need a new instance (but the C# compiler won't let us do a cast so we have to Script.Write it)
+					return Script.Write<ResultOrError<TDestination>>("this");
+				}
+				return ResultOrError<TDestination>.FromError(_errorMessage.Value); // If there is no result then there must be an error
+			}
+
+			var newResult = mapper(_result.Value);
+			if (newResult == null)
+				throw new ArgumentException($"The provided {nameof(mapper)} returned null, which is not valid");
+
+			if ((typeof(T) == typeof(TDestination)) && newResult.Equals(_result.Value))
+			{
+				// If the destination type is the same as the current type and the new value is the same as the current one then we don't need a new instance (but the C# compiler won't let us
+				// do a cast so we have to Script.Write it)
+				return Script.Write<ResultOrError<TDestination>>("this");
+			}
+			return new ResultOrError<TDestination>(newResult, _errorMessage);
+		}
+	}
+}
