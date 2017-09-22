02da0b197535766a7fed7754abe62178206427c9
ProductiveRage <dangger36@gmail.com>
2017-09-22 10:20:49 +0100

    Added ResultOrError<T> "ReplaceMissingResultWithError" extension method

---
+++ ProductiveRage.Immutable.Extensions/ResultOrErrorExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ProductiveRage.Immutable
+{
+	public static class ResultOrErrorExtensions
+	{
+		/// <summary>
+		/// Given a ResultOrError of Optional T, return a ResultOrError of T by replacing a Missing T value with an Error ResultOrError of T
+		/// </summary>
+		public static ResultOrError<T> ReplaceMissingResultWithError<T>(this ResultOrError<Optional<T>> source, NonBlankTrimmedString errorMessageForMissingValue)
+		{
+			if (source == null)
+				throw new ArgumentNullException(nameof(source));
+			if (errorMessageForMissingValue == null)
+				throw new ArgumentNullException(nameof(errorMessageForMissingValue));
+
+			return source.Match(
+				handleResult: resultIfAvailable => resultIfAvailable
+					.Map(result => ResultOrError.FromResult(result))
+					.GetValueOrDefault(ResultOrError<T>.FromError(errorMessageForMissingValue)),
+				handleError: errorMessage => ResultOrError<T>.FromError(errorMessage)
+			);
+		}
+	}
+}
