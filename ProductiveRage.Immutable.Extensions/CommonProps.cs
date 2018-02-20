e2f70ff3be6ad832d53049e46e74e08e658ec299
ProductiveRage <dangger36@gmail.com>
2018-02-20 19:41:40 +0000

    (2.6.0) Remove [ObjectLiteral] and [IgnoreGeneric] from CommonProps (Bridge 16 meant they caused problems when used as Bridge.React props)

--- ProductiveRage.Immutable.Extensions/CommonProps.cs
+++ ProductiveRage.Immutable.Extensions/CommonProps.cs
@@ -1,86 +1,50 @@
 ﻿using System;
 using Bridge;
+using Bridge.React;
 
 namespace ProductiveRage.Immutable
 {
-	[ObjectLiteral]
-	public sealed class CommonProps<T>
+	// 2018-02-20 DWR: This used to be an [ObjectLiteral] for eking out a small performance improvement but Bridge (16, I think) has meant that this won't work properly any more when used
+	// as Component props class and so [ObjectLiteral] is being removed (I'll leave the constructor private since we don't need to change the interface)
+	public sealed class CommonProps<T> : PropsWithKey
 	{
-		/// <summary>
-		/// Since this is an [ObjectLiteral] class, its constructor would never be executed so the input validation has been moved to the BuildCommonPropsObjectLiteral method in the
-		/// non-generic class below which is now the only way to construct an instance of this class (so the constructor is private since BuildCommonPropsObjectLiteral directly writes
-		/// out JavaScript to create an object literal with the required data). This class remains as an important part of the type system, despite its weird construction. We shouldn't
-		/// mark is as [External] since then it won't be possible to use it as a type parameter (which it may be when using it as the props type for component classes).
-		/// </summary>
-		private CommonProps() { }
-
-		public T Model { [Template("model")]get; }
-		public Action<T> OnChange { [Template("onChange")]get; }
-		public Optional<ClassName> ClassName { [Template("className")]get; }
-		public bool Disabled { [Template("disabled")]get; }
-
-		// 2017-04-28 DWR: I'd like to rename all of the "state" properties in the construction methods below but that could break existing code too (maybe one day in the future)
-		[Obsolete("The 'State'property was confusingly-named since it did not relate to the React concept of State (where a Component has a Props reference and a State instance) and so it has been renamed to Model (as in View Model) - this property is an alias on to Model and exists for backwards compatibility but will be removed in a future version of the library")]
-		public T State { [Template("model")]get; }
-
-		// A Key property may be set by the CommonProps initialisation below but there is no need to expose that information since it is for React's benefit only (so there is no "Key"
-		// property here)
-	}
-
-	public static class CommonProps
-	{
-		[IgnoreGeneric]
-		public static CommonProps<T> For<T>(T state, Action<T> onChange)
-		{
-			return BuildCommonPropsObjectLiteral<T>(state, onChange, Optional<ClassName>.Missing, false, null);
-		}
-
-		[IgnoreGeneric]
-		public static CommonProps<T> For<T>(T state, Action<T> onChange, Union<string, int> key)
-		{
-			return BuildCommonPropsObjectLiteral<T>(state, onChange, Optional<ClassName>.Missing, false, key);
-		}
-
-		[IgnoreGeneric]
-		public static CommonProps<T> For<T>(T state, Action<T> onChange, Optional<ClassName> className, bool disabled)
-		{
-			return BuildCommonPropsObjectLiteral<T>(state, onChange, className, disabled, null);
-		}
-
-		[IgnoreGeneric]
-		public static CommonProps<T> For<T>(T state, Action<T> onChange, string classNameIfAny, bool disabled)
-		{
-			var className = string.IsNullOrWhiteSpace(classNameIfAny) ? Optional<ClassName>.Missing : new ClassName(classNameIfAny);
-			return BuildCommonPropsObjectLiteral<T>(state, onChange, className, disabled, null);
-		}
-
-		[IgnoreGeneric]
-		public static CommonProps<T> For<T>(T state, Action<T> onChange, Optional<ClassName> className, bool disabled, Union<string, int> key)
-		{
-			return BuildCommonPropsObjectLiteral<T>(state, onChange, className, disabled, key);
-		}
-
-		[IgnoreGeneric]
-		public static CommonProps<T> For<T>(T state, Action<T> onChange, string classNameIfAny, bool disabled, Union<string, int> key)
-		{
-			var className = string.IsNullOrWhiteSpace(classNameIfAny) ? Optional<ClassName>.Missing : new ClassName(classNameIfAny);
-			return BuildCommonPropsObjectLiteral<T>(state, onChange, className, disabled, key);
-		}
-
-		[IgnoreGeneric]
-		private static CommonProps<T> BuildCommonPropsObjectLiteral<T>(T model, Action<T> onChange, Optional<ClassName> className, bool disabled, Union<string, int> key)
+		public CommonProps(Optional<ClassName> className, bool disabled, T model, Action<T> onChange, Union<int, string> key) : base(key)
 		{
 			if (model == null)
 				throw new ArgumentNullException(nameof(model));
 			if (onChange == null)
 				throw new ArgumentNullException(nameof(onChange));
 
-			// Only include a "key" property if key is non-null (React interprets null as a key value and if multiple components have a null key then they will be considered
-			// to both have the same key, as opposed to them both having NO key, which we want to avoid);
-			var props = Script.Write<CommonProps<T>>("{ model: model, onChange: onChange, className: className, disabled: disabled }");
-			if (key != null)
-				Script.Write("{0}.key = {1}", props, key);
-			return props;
+			ClassName = className;
+			Disabled = disabled;
+			Model = model;
+			OnChange = onChange;
+		}
+
+		public Optional<ClassName> ClassName { get; }
+		public bool Disabled { get; }
+		public T Model { get; }
+		public Action<T> OnChange { get; }
+		
+		// 2017-04-28 DWR: I'd like to rename all of the "state" properties in the construction methods below but that could break existing code too (maybe one day in the future)
+		[Obsolete("The 'State'property was confusingly-named since it did not relate to the React concept of State (where a Component has a Props reference and a State instance) and so it has been renamed to Model (as in View Model) - this property is an alias on to Model and exists for backwards compatibility but will be removed in a future version of the library")]
+		public T State { get { return Model; } }
+	}
+
+	public static class CommonProps
+	{
+		// 2018-02-20 DWR: These used to use [IgnoreGeneric] for small performance improvements (which were less relevant after Bridge made changes to how they dealt with generic) but it's safer to remove them
+
+		public static CommonProps<T> For<T>(T model, Action<T> onChange) { return new CommonProps<T>(null, false, model, onChange, null); }
+		public static CommonProps<T> For<T>(T model, Action<T> onChange, Union<int, string> key) { return new CommonProps<T>(null, false, model, onChange, key); }
+		public static CommonProps<T> For<T>(T model, Action<T> onChange, Optional<ClassName> className, bool disabled) { return new CommonProps<T>(className, disabled, model, onChange, null); }
+		public static CommonProps<T> For<T>(T model, Action<T> onChange, string classNameIfAny, bool disabled) { return new CommonProps<T>(ToNonBlankTrimmedString(classNameIfAny), disabled, model, onChange, null); }
+		public static CommonProps<T> For<T>(T model, Action<T> onChange, Optional<ClassName> className, bool disabled, Union<int, string> key) { return new CommonProps<T>(className, disabled, model, onChange, key); }
+		public static CommonProps<T> For<T>(T model, Action<T> onChange, string classNameIfAny, bool disabled, Union<int, string> key) { return new CommonProps<T>(ToNonBlankTrimmedString(classNameIfAny), disabled, model, onChange, key); }
+
+		private static Optional<ClassName> ToNonBlankTrimmedString(string value)
+		{
+			return string.IsNullOrWhiteSpace(value) ? null : new ClassName(value);
 		}
 	}
 }
