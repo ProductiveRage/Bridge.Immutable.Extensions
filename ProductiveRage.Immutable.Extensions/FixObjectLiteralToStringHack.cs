880df815e899f07269e893a38593faa97e2ac909
ProductiveRage <dangger36@gmail.com>
2019-01-11 16:45:37 +0000

    (4.1.0) Add a workaround to make [ObjectLiteral] classes' custom ToString() methods respected
    I think that this is a bug with Bridge but it's still up for debate -
    I'm including this workaround here because the NonBlankTrimmedString
    class needs it and so this seems like a relatively reasonable place for
    it

---
+++ ProductiveRage.Immutable.Extensions/FixObjectLiteralToStringHack.cs
@@ -0,0 +1,41 @@
+﻿using Bridge;
+
+namespace ProductiveRage.Immutable
+{
+	internal sealed class FixObjectLiteralToStringHack
+	{
+		/// <summary>
+		/// This is a workaround for [ObjectLiteral] types that don't get the same ToString() treatment as other types do. Hopefully Bridge will fix this properly, I've raised an issue about it:
+		/// https://forums.bridge.net/forum/community/help/6001 - in the meantime, having this code here will mean that any projects that pulls in this library should find that [ObjectLiteral]
+		/// types with custom ToString() methods (such as NonBlankTrimmedString) work correctly when referenced in string interpolation calls.
+		/// </summary>
+		[Init]
+		private static void ApplyWorkaround()
+		{
+			/*@
+			(function () {
+				var bridgeToString = Bridge.toString;
+				if (bridgeToString.haveManipulatedMethodFlagName) {
+					// Just in case this workaround code gets included in multiple places and executed multiple times, only intercept the
+					// method once and do nothing on any subsequent calls
+					return;
+				}
+				Bridge.toString = function (value) {
+					if (value) {
+						var type = Bridge.getType(value);
+						var identifiedAsObjectLiteral = type.$literal === true;
+						if (identifiedAsObjectLiteral) {
+							var toStringMethod = type.prototype.toString;
+							if (!Bridge.referenceEquals(toStringMethod, Object.prototype.toString)) {
+								return toStringMethod.apply(value);
+							}
+						}
+					}
+					return bridgeToString(value);
+				};
+				Bridge.toString.haveManipulatedMethodFlagName = true;
+			}());
+			*/
+		}
+	}
+}
