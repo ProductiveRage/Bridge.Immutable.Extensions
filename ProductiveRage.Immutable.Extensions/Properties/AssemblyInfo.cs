22c967411f1d356ffe470e7c1c3e80d7a0d8a1d8
Dan Roberts <dangger36@gmail.com>
2019-07-03 19:25:11 +0100

    Should have included this in the last changeset

--- ProductiveRage.Immutable.Extensions/Properties/AssemblyInfo.cs
+++ ProductiveRage.Immutable.Extensions/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("ProductiveRage.Immutable.Extensions")]
-[assembly: AssemblyCopyright("Copyright © ProductiveRage 2017")]
+[assembly: AssemblyCopyright("Copyright © ProductiveRage 2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("5.1.0.0")]
-[assembly: AssemblyFileVersion("5.1.0.0")]
+[assembly: AssemblyVersion("5.2.0.0")]
+[assembly: AssemblyFileVersion("5.2.0.0")]
